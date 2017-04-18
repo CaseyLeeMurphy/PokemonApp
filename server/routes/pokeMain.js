e755414dc6854dc7f5dddd40646d011515bd31fa
Casey Lee Murphy <caseyleemurphy@ldschurch.org>
2017-04-18 12:29:31 -0600

    Initial work for pokemon app

--- server/routes/pokeMain.js
+++ server/routes/pokeMain.js
@@ -21,16 +21,7 @@ module.exports = router;
 
 // --------------------------------------- Post ---------------------------------------------------
 // ------------------------------------------------------------------------------------------------
-router.post("/api/v1/students", function (req, res) {
-    studentData.post(req.body, (err, result) => {
-        if (err) {
-            res.status(500).send("There was a problem inserting that record into the database");
-        } else {
-            console.log("Inserted student with newID of " + result);
-            res.status(200).send(result);
-        }    
-    });
-});
+
 
 // ---------------------------------------- Get ---------------------------------------------------
 // ------------------------------------------------------------------------------------------------
@@ -48,43 +39,9 @@ router.get("/api/v1/pokemon/:id", function(req, res) {
 
 // --------------------------------------- Update/Put ---------------------------------------------
 // ------------------------------------------------------------------------------------------------
-router.put("/api/v1/students/:id", function (req, res) {
-    var id = req.params.id;
-    var data = req.body;
-    
-    studentData.update(id, data, (err, result) => {
-        if (err) {
-            res.status(500).send("There was a problem updating that ID in the database");
-        } else {
-            console.log("Updated student with ID" + id);
-            res.status(204).send();
-        }    
-    });
-});
 
 // --------------------------------------- Delete -------------------------------------------------
 // ------------------------------------------------------------------------------------------------
-router.delete("/api/v1/students/:id", function (req, res) {
-    var id = req.params.id;
-
-    studentData.delete(id, (err, result) => {
-        if (err) {
-            res.status(500).send("There was a problem deleting that ID from the database");
-        } else {
-            console.log("Deleted student with ID" + id);
-            res.status(204).send();
-        }    
-    });
-});
 
 // --------------------------------------- List ---------------------------------------------------
 // ------------------------------------------------------------------------------------------------
-router.get("/api/v1/students", function(req, res) {
-    studentData.list((err, result) => {
-        if (err) {
-            res.status(500).send("There was a problem getting students from the database");
-        } else {
-            res.status(201).send(result);
-        }    
-    });
-});
