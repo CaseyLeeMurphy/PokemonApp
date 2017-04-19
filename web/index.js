4a56bd6d82a5ebf33b39e4fda3fd889f1210a0d9
Daniel Clark <danielsan88@gmail.com>
2017-04-19 13:07:03 -0600

    Add types display to DetailsDialog

--- web/index.js
+++ web/index.js
@@ -39,8 +39,7 @@ app.controller('pokeMainController', function($scope, $http, $mdDialog){
                             })
                         }
                     ]
-                },
-                options: {}
+                }
             });
         },function(err) {
             console.log('There was an error: ' + err);
@@ -70,6 +69,13 @@ app.controller('pokeMainController', function($scope, $http, $mdDialog){
             $scope.pokemon = pokemon;
             $scope.inParty = inParty;
             $scope.index = index;
+
+            $scope.type1 = pokemon.types.find(t => t.slot === 1).type.name;
+            let type2 = pokemon.types.find(t => t.slot === 2);
+            $scope.type2 = type2 ? type2.type.name : null;
+
+            $scope.type1Style = { backgroundColor: PokeTypes.colors[$scope.type1], color: 'black'};
+            $scope.type2Style = { backgroundColor: type2 ? PokeTypes.colors[$scope.type2] : null, color: 'black'};
 
             $scope.hide = function() {
                 $mdDialog.hide();
